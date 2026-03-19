b6c842e6d34bbbf92960afb4593b51f3dbe104ec
SisaDube88 <dube.sisa88@gmail.com>
2026-03-20 00:10:15 +0200

    Add files via upload

---
+++ main_.py
@@ -0,0 +1,12 @@
+# main.py
+
+# Import the math_operations module
+import math_operations
+
+# Use the add function
+result_add = math_operations.add(5, 7)
+print(f"5 + 7 = {result_add}")
+
+# Use the multiply function
+result_multiply = math_operations.multiply(5, 7)
+print(f"5 * 7 = {result_multiply}")
---
+++ math_operations.py
@@ -0,0 +1,9 @@
+# math_operations.py
+
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+# Function to multiply two numbers
+def multiply(a, b):
+    return a * b
